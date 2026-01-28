3bf8b40bbf7817983d6b6eadfe6409b2b8b406b7
Florian Sager <sager@agitos.de>
2026-01-28 09:38:43 +0100

    Update DLL export definition to support Darwin platform

--- include/itm.h
+++ include/itm.h
@@ -7,7 +7,7 @@
 using namespace std;
 
 // Export the DLL functions as "C" and not C++
-#ifdef _LINUX
+#if defined(_LINUX) || defined(_DARWIN)
 #define DLLEXPORT extern "C"
 #else
 #define DLLEXPORT extern "C" __declspec(dllexport)
